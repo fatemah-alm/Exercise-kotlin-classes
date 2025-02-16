93011cdec0052ed49724ae4aba560aeb0c6e7aab
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-16 18:28:03 +0300

    Made class book with constructor

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    println("Hello World!")
+
+    val book = Book(title ="Python for dummies",author = "John Paul Mueller" , yearPublished = 2006)
+//    book.title = "Python for dummies"
+//    book.author = "John Paul Mueller"
+//    book.yearPublished = 2006
+}
+
+class Book(var title: String,var author: String, var yearPublished: Int  ){
 }
