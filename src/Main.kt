b76b467e6c3e0175d5ee6c0202b344b5c99a9e1f
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-16 18:38:55 +0300

    Added genre

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,14 @@
 fun main() {
 
-    val book = Book(title ="Python for dummies",author = "John Paul Mueller" , yearPublished = 2006)
+    val book = Book(title ="Python for dummies",author = "John Paul Mueller" , yearPublished = 2006, genre = "Coding" )
 //    book.title = "Python for dummies"
 //    book.author = "John Paul Mueller"
 //    book.yearPublished = 2006
     book.getBookInfo()
 }
 
-class Book(var title: String,var author: String, var yearPublished: Int  ){
+class Book(var title: String,var author: String, var yearPublished: Int, var genre: String){
     fun getBookInfo(){
-        println("$title by $author $yearPublished")
+        println("$title by $author $yearPublished - $genre")
     }
 }
